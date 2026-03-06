6c884f668e50733405e1cbf7c08883e5d737c466
Samuel Thompson <samthompson1818@gmail.com>
2026-03-06 11:51:05 -0500

    Added tile location to unit class

--- core/src/main/java/me/jamboxman5/natac/units/Unit.java
+++ core/src/main/java/me/jamboxman5/natac/units/Unit.java
@@ -1,15 +1,22 @@
 package me.jamboxman5.natac.units;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import me.jamboxman5.natac.map.tile.Tile;
 import space.earlygrey.shapedrawer.ShapeDrawer;
 
 public abstract class Unit {
     int speed;
     int range;
 
-    protected Unit(int speed, int range) {
+    float x;
+    float y;
+
+    Tile location;
+
+    protected Unit(int speed, int range, Tile location) {
         this.speed = speed;
         this.range = range;
+        this.location = location;
     }
 
     public abstract void update();
