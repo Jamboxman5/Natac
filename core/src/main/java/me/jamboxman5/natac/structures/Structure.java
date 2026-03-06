b2ce2a88bf1128dd5f35888128ad66410e14963e
Samuel Thompson <samthompson1818@gmail.com>
2026-03-06 11:39:07 -0500

    Converted Unit & Tile to abstract classes, created constructors

--- core/src/main/java/me/jamboxman5/natac/structures/Structure.java
+++ core/src/main/java/me/jamboxman5/natac/structures/Structure.java
@@ -1,6 +1,14 @@
 package me.jamboxman5.natac.structures;
 
-public class Structure {
+public abstract class Structure {
     int buildCost;
     int revenuePerTurn;
+
+    protected Structure(int buildCost, int revenuePerTurn) {
+        this.buildCost = buildCost;
+        this.revenuePerTurn = revenuePerTurn;
+    }
+
+    public abstract void update();
+    public abstract void draw();
 }
--- core/src/main/java/me/jamboxman5/natac/units/Unit.java
+++ core/src/main/java/me/jamboxman5/natac/units/Unit.java
@@ -1,6 +1,14 @@
 package me.jamboxman5.natac.units;
 
-public class Unit {
+public abstract class Unit {
     int speed;
     int range;
+
+    protected Unit(int speed, int range) {
+        this.speed = speed;
+        this.range = range;
+    }
+
+    public abstract void update();
+    public abstract void draw();
 }
