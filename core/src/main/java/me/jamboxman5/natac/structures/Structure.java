80bfc5111bf40af6f4bdc9c88c5f1685dbafd4cf
Samuel Thompson <samthompson1818@gmail.com>
2026-03-06 19:05:32 -0500

    Added location tile market to structure class

--- core/src/main/java/me/jamboxman5/natac/structures/Structure.java
+++ core/src/main/java/me/jamboxman5/natac/structures/Structure.java
@@ -1,15 +1,18 @@
 package me.jamboxman5.natac.structures;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import me.jamboxman5.natac.map.tile.Tile;
 import space.earlygrey.shapedrawer.ShapeDrawer;
 
 public abstract class Structure {
-    int buildCost;
-    int revenuePerTurn;
+    protected int buildCost;
+    protected int revenuePerTurn;
+    protected Tile location;
 
-    protected Structure(int buildCost, int revenuePerTurn) {
+    protected Structure(int buildCost, int revenuePerTurn, Tile location) {
         this.buildCost = buildCost;
         this.revenuePerTurn = revenuePerTurn;
+        this.location = location;
     }
 
     public abstract void update();
