158d0c301c9696c4fb9aee952a2a42abf4e0d927
Samuel Thompson <samthompson1818@gmail.com>
2026-03-06 19:07:29 -0500

    Added coordinate vector to structure class

--- core/src/main/java/me/jamboxman5/natac/structures/Structure.java
+++ core/src/main/java/me/jamboxman5/natac/structures/Structure.java
@@ -1,6 +1,7 @@
 package me.jamboxman5.natac.structures;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import me.jamboxman5.natac.map.tile.Tile;
 import space.earlygrey.shapedrawer.ShapeDrawer;
 
@@ -9,10 +10,14 @@ public abstract class Structure {
     protected int revenuePerTurn;
     protected Tile location;
 
+    protected Vector2 position;
+
     protected Structure(int buildCost, int revenuePerTurn, Tile location) {
         this.buildCost = buildCost;
         this.revenuePerTurn = revenuePerTurn;
         this.location = location;
+
+        this.position = location.getTilePosition();
     }
 
     public abstract void update();
