5baa7fa5824ce88b405a2b6c64eb22063a8de304
Samuel Thompson <samthompson1818@gmail.com>
2026-05-09 21:24:23 -0400

    Created lobby screen

--- core/src/main/java/me/jamboxman5/natac/screen/LobbyScreen.java
+++ core/src/main/java/me/jamboxman5/natac/screen/LobbyScreen.java
@@ -2,11 +2,12 @@ package me.jamboxman5.natac.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -22,16 +23,16 @@ import me.jamboxman5.natac.player.Player;
 import me.jamboxman5.natac.screen.ui.Fonts;
 import me.jamboxman5.natac.util.Settings;
 
+
 import javax.swing.*;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class MainMenuScreen implements Screen {
+public class LobbyScreen implements Screen {
 
     Texture menuBKG;
 
     OrthographicCamera camera;
-
-    int move = 600;
 
     private SpriteBatch spriteBatch;
     private ShapeRenderer shapes;
@@ -41,8 +42,12 @@ public class MainMenuScreen implements Screen {
 
     private final Viewport viewport;
 
+    private ArrayList<Player> players;
 
-    public MainMenuScreen() {
+    public LobbyScreen(Player player) {
+
+        players = new ArrayList<>();
+        players.add(player);
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Settings.screenWidth, Settings.screenHeight);
@@ -52,47 +57,41 @@ public class MainMenuScreen implements Screen {
 
         shapes = new ShapeRenderer();
         spriteBatch = new SpriteBatch();
+
         elements = new Table();
         elements.setFillParent(true);
+
         uiStage.addActor(elements);
 
         Skin skin = new Skin(Gdx.files.internal("ui/skins/expee/expee-ui.json"));
 
-        TextButton button1 = new TextButton("Host Game", skin);
-        TextButton button2 = new TextButton("Join Game", skin);
-        TextField field = new TextField("", skin);
+        TextButton button1 = new TextButton("Start Game", skin);
+        TextButton button2 = new TextButton("Quit Game", skin);
 
-        elements.add(field).fillX().center();
-        elements.row();
         elements.add(button1);
         elements.add(button2);
         elements.row();
-        field.setMessageText("Enter name: ");
 
         elements.setDebug(false);
         button1.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                if (field.getText().isEmpty()) return;
-                Natac.instance.setScreen(new LobbyScreen(new Player(field.getText(), Color.RED)));
+                Natac.instance.setScreen(new GameScreen(player, new ArrayList<>(), "localhost"));
             }
         });
 
         button2.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                if (field.getText().isEmpty()) return;
-                String ip = JOptionPane.showInputDialog("Enter host IP: ");
-                Natac.instance.setScreen(new GameScreen(new Player(field.getText(), Color.RED), new ArrayList<>(), ip));
+                Natac.instance.setScreen(new MainMenuScreen());
             }
         });
+
     }
 
     @Override
     public void show() {
         Gdx.input.setInputProcessor(uiStage);
-
-
     }
 
     @Override
@@ -100,19 +99,12 @@ public class MainMenuScreen implements Screen {
         ScreenUtils.clear(0f, 0, 0f, 1);
 
         camera.update();
-        shapes.setProjectionMatrix(camera.combined);
         spriteBatch.setProjectionMatrix(camera.combined);
+        shapes.setProjectionMatrix(camera.combined);
 
-//        Fonts.drawScaled(Fonts.SUBTITLEFONT, .85f, "Zombie Assault", game.batch,20, ScreenInfo.HEIGHT - Fonts.TITLEFONT.getScaleY() - Fonts.SUBTITLEFONT.getScaleY() - 220);
 
         update(delta);
         draw();
-
-
-    }
-
-    public void update(float delta) {
-        uiStage.act(delta);
     }
 
     @Override
@@ -140,13 +132,41 @@ public class MainMenuScreen implements Screen {
         menuBKG.dispose();
     }
 
-    public void draw() {
-        uiStage.draw();
 
-        spriteBatch.begin();
-        Fonts.PLACEHOLDER_FONT.draw(spriteBatch, "NATAC", Fonts.getXForCenteredText(Settings.screenWidth / 2, "NATAC", Fonts.PLACEHOLDER_FONT), 500);
-        spriteBatch.end();
+
+    public void update(float delta) {
+        uiStage.act(delta);
     }
 
+    public void drawBKG(SpriteBatch batch) {
+        batch.begin();
+//        batch.draw(menuBKG, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
+    }
+
+    public void drawTitle(SpriteBatch batch) {
+        batch.begin();
+
+        batch.end();
+    }
+
+    public void draw() {
+
+        drawBKG(spriteBatch);
+        drawTitle(spriteBatch);
+        uiStage.draw();
+        spriteBatch.begin();
+        Fonts.PLACEHOLDER_FONT.draw(spriteBatch, "Lobby", Fonts.getXForCenteredText(Settings.screenWidth / 2, "NATAC", Fonts.PLACEHOLDER_FONT), 500);
+        Fonts.PLACEHOLDER_FONT.draw(spriteBatch, "Players: ", Fonts.getXForCenteredText(Settings.screenWidth / 2, "NATAC", Fonts.PLACEHOLDER_FONT), 200);
+
+        int y = 200;
+
+        for (Player p : players) {
+            Fonts.PLACEHOLDER_FONT.draw(spriteBatch, p.getUsername(), Fonts.getXForCenteredText(Settings.screenWidth / 2, "NATAC", Fonts.PLACEHOLDER_FONT) + 100, y);
+            y -= 30;
+        }
+
+        spriteBatch.end();
+    }
 
 }
