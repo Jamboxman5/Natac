ab6c56704264c2c1f5a576c7472a09da59d39e9d
Samuel Thompson <samthompson1818@gmail.com>
2026-05-09 20:35:53 -0400

    Added reference to current playable map within Card interface

--- core/src/main/java/me/jamboxman5/natac/cards/Card.java
+++ core/src/main/java/me/jamboxman5/natac/cards/Card.java
@@ -1,9 +1,10 @@
 package me.jamboxman5.natac.cards;
 
+import me.jamboxman5.natac.map.Map;
 import me.jamboxman5.natac.player.Player;
 
 public interface Card {
 
-    void playCard(Player playing);
+    void playCard(Player playing, Map currentMap);
 
 }
--- core/src/main/java/me/jamboxman5/natac/cards/ExampleCard.java
+++ core/src/main/java/me/jamboxman5/natac/cards/ExampleCard.java
@@ -1,5 +1,7 @@
 package me.jamboxman5.natac.cards;
 
+import me.jamboxman5.natac.map.Map;
+import me.jamboxman5.natac.map.tile.Tile;
 import me.jamboxman5.natac.player.Player;
 
 public class ExampleCard implements Card {
@@ -9,7 +11,7 @@ public class ExampleCard implements Card {
     int exampleValue = 2;
 
     @Override
-    public void playCard(Player player) {
+    public void playCard(Player player, Map currentMap) {
         //Define effects to happen when card is played
         //Player variable is the instance of the player who is playing this card
 
