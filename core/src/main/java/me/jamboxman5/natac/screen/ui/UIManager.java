7786c6d6266c4b8c4ccd275ae1ebe7760fa38158
Samuel Thompson <samthompson1818@gmail.com>
2026-05-19 22:20:59 -0400

    Started drawing player stats / hud information in the play UI

--- core/src/main/java/me/jamboxman5/natac/screen/ui/UIManager.java
+++ core/src/main/java/me/jamboxman5/natac/screen/ui/UIManager.java
@@ -3,12 +3,17 @@ package me.jamboxman5.natac.screen.ui;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import me.jamboxman5.natac.Natac;
 import me.jamboxman5.natac.screen.GameScreen;
+import me.jamboxman5.natac.util.Settings;
 import space.earlygrey.shapedrawer.ShapeDrawer;
 
 public class UIManager {
 
     public static void draw(SpriteBatch batch, ShapeDrawer shapes, GameScreen.State gameState) {
         Fonts.drawScaled(Fonts.PLACEHOLDER_FONT, 1f, Natac.instance.player.getUsername(), batch, 20, 40);
+
+        int statStart = Settings.screenHeight - 40;
+
+        Fonts.drawScaled(Fonts.PLACEHOLDER_FONT, 1f, "Money: $" + Natac.instance.player.getMoney(), batch, 20, statStart);
 
         switch(gameState) {
             case PLAY:
@@ -20,6 +25,7 @@ public class UIManager {
             case CLAIM:
                 Fonts.drawScaled(Fonts.PLACEHOLDER_FONT, 1f, "Select a starting tile. ", batch, 20, 80);
                 break;
+
         }
 
     }
