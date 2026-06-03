df5bef9b181acb30262f86dd85c705fee8c02f0e
Samuel Thompson <samthompson1818@gmail.com>
2026-06-03 15:59:02 -0400

    Started implementation of drag and drop building placement

--- core/src/main/java/me/jamboxman5/natac/screen/ui/modal/SelectedTileModal.java
+++ core/src/main/java/me/jamboxman5/natac/screen/ui/modal/SelectedTileModal.java
@@ -7,13 +7,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import me.jamboxman5.natac.Natac;
 import me.jamboxman5.natac.map.tile.Tile;
@@ -39,8 +40,17 @@ public class SelectedTileModal extends Stage {
     Button buildButton;
     Button backButton;
 
-    SelectBox<StructureSelection> buildSelector;
-    Button buildConfirmButton;
+
+    DragAndDrop dragAndDrop;
+    ScrollPane scrollPane;
+    Table buttonOrganizer;
+
+    Array<Button> popupButtons;
+
+    int margin = 40;
+
+    Vector2 tileCenter = new Vector2(Settings.screenWidth / 2f, (Settings.screenHeight / 2f) + 50);
+
 
     public SelectedTileModal(Tile t) {
 
@@ -52,7 +62,6 @@ public class SelectedTileModal extends Stage {
 
         int width = Settings.screenWidth / 10;
         int height = Settings.screenHeight / 10;
-        int margin = 40;
 
         float x = (Settings.screenWidth / 2f) - (margin / 2f) - width;
 
@@ -62,13 +71,71 @@ public class SelectedTileModal extends Stage {
         addActor(buildButton);
         addActor(backButton);
 
-        buildSelector = new SelectBox<>(skin);
-        buildSelector.setItems(StructureSelection.NONE, StructureSelection.BARRACKS);
-        buildSelector.setSize(width, height);
-        buildSelector.setPosition(x + (width) + (margin), margin * 2.5f);
+        createBuildMenu();
 
-        buildConfirmButton = getButton("Confirm", getBuildConfirmAction(), width, height, x + (width * 2) + (margin * 2), margin * 2.5f);
+    }
 
+    private void createBuildMenu() {
+
+        dragAndDrop = new DragAndDrop();
+
+        buttonOrganizer = new Table();
+        buttonOrganizer.top();
+
+        Button bb = new TextButton("Barracks", skin);
+        Button bb2 = new TextButton("Butt", skin);
+        Button bb3 = new TextButton("Butt", skin);
+        Button bb4 = new TextButton("Butt", skin);
+        Button bb5 = new TextButton("Butt", skin);
+        Button bb6 = new TextButton("Butt", skin);
+
+        buttonOrganizer.add(bb).width(290).height(200).pad(5).row();
+        buttonOrganizer.add(bb2).width(290).height(200).pad(5).row();
+        buttonOrganizer.add(bb3).width(290).height(200).pad(5).row();
+        buttonOrganizer.add(bb4).width(290).height(200).pad(5).row();
+        buttonOrganizer.add(bb5).width(290).height(200).pad(5).row();
+        buttonOrganizer.add(bb6).width(290).height(200).pad(5).row();
+
+        dragAndDrop.addSource(new DragAndDrop.Source(bb) {
+            @Override
+            public DragAndDrop.Payload dragStart(
+                InputEvent event,
+                float x,
+                float y,
+                int pointer) {
+
+                DragAndDrop.Payload payload = new DragAndDrop.Payload();
+                payload.setObject(StructureSelection.BARRACKS);
+
+                return payload;
+            }
+
+            @Override
+            public void dragStop(
+                InputEvent event,
+                float x,
+                float y,
+                int pointer,
+                DragAndDrop.Payload payload,
+                DragAndDrop.Target target) {
+
+                StructureSelection selected = (StructureSelection) payload.getObject();
+                Vector2 dropPos = new Vector2(Gdx.input.getX(), Settings.screenHeight - Gdx.input.getY());
+
+                PacketUtil.buildStructure(new Barracks(Natac.instance.player.getPlayerClass(), selectedTile.getTilePosition(), unprojectDropPos(dropPos)), selectedTile.getTilePosition());
+                PacketUtil.createStatChange(Natac.instance.player, 0, 0, 0, 0, -selected.goldCost, -selected.resourceCost);
+            }
+        });
+
+        scrollPane = new ScrollPane(buttonOrganizer);
+        scrollPane.setSize(300, Settings.screenHeight - (margin * 2));
+        scrollPane.setPosition(Settings.screenWidth - 300 - margin, margin);
+        scrollPane.setScrollingDisabled(true, false);
+
+    }
+
+    private Vector2 unprojectDropPos(Vector2 dropPos) {
+        return dropPos.cpy().sub(tileCenter).scl(1f/5f);
     }
 
     private float bgAlpha = 0f;
@@ -81,7 +148,7 @@ public class SelectedTileModal extends Stage {
         shapes.setColor(new Color(0,0,0, bgAlpha));
         shapes.filledRectangle(0, 0, Settings.screenWidth, Settings.screenHeight);
 
-        selectedTileSprite.setCenter(Settings.screenWidth / 2f, (Settings.screenHeight / 2f) + 50);
+        selectedTileSprite.setCenter(tileCenter.x, tileCenter.y);
         selectedTileSprite.setScale(5f * 0.9f, 5f * 0.9f);
         selectedTileSprite.draw(batch);
 
@@ -92,16 +159,16 @@ public class SelectedTileModal extends Stage {
         if (selectedTile.getType() == TileType.MOUNTAINS) {
             Sprite layer = new Sprite(Tile.mountainsLayer);
             layer.setScale(5f * 0.9f);
-            layer.setCenter(Settings.screenWidth / 2f, (Settings.screenHeight / 2f) + 50);
+            layer.setCenter(tileCenter.x, tileCenter.y);
             layer.draw(batch);
         }
 
         for (Structure structure : selectedTile.getStructures()) {
-            structure.drawModal(batch, shapes, new Vector2(Settings.screenWidth / 2f, (Settings.screenHeight / 2f) + 50));
+            structure.drawModal(batch, shapes, tileCenter);
         }
 
         for (Unit unit : selectedTile.getUnits()) {
-            unit.drawModal(batch, shapes, new Vector2(Settings.screenWidth / 2f, (Settings.screenHeight / 2f) + 50));
+            unit.drawModal(batch, shapes, tileCenter);
         }
 
         draw();
@@ -151,58 +218,67 @@ public class SelectedTileModal extends Stage {
         return new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                buildButton.remove();
-                addActor(buildSelector);
-                addActor(buildConfirmButton);
+//                buildButton.remove();
+                addActor(scrollPane);
+                setScrollFocus(scrollPane);
             }
         };
     }
 
-    public ChangeListener getBuildConfirmAction() {
-        return new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-
-                StructureSelection building = buildSelector.getSelected();
-
-                int cost = 0;
-                Structure toBuild = null;
-
-                switch(building) {
-                    case NONE:
-                        buildSelector.remove();
-                        buildConfirmButton.remove();
-                        addActor(buildButton);
-                        break;
-                    case BARRACKS:
-                        cost = Barracks.goldCost;
-                        toBuild = new Barracks(Natac.instance.player.getPlayerClass(), selectedTile.getTilePosition(), new Vector2());
-                        break;
-                }
-
-                if (toBuild == null || Natac.instance.player.getGold() < cost) return;
-
-                buildSelector.remove();
-                buildConfirmButton.remove();
-                addActor(buildButton);
-
-                PacketUtil.buildStructure(toBuild, selectedTile.getTilePosition());
-                PacketUtil.createStatChange(Natac.instance.player, 0, 0, 0, 0, -cost, 0);
-            }
-        };
-    }
+//    public ChangeListener getBuildConfirmAction() {
+//        return new ChangeListener() {
+//            @Override
+//            public void changed(ChangeEvent event, Actor actor) {
+//
+//                StructureSelection building = buildSelector.getSelected();
+//
+//                int cost = 0;
+//                Structure toBuild = null;
+//
+//                switch(building) {
+//                    case NONE:
+//                        buildSelector.remove();
+//                        buildConfirmButton.remove();
+//                        addActor(buildButton);
+//                        break;
+//                    case BARRACKS:
+//                        cost = Barracks.goldCost;
+//                        toBuild = new Barracks(Natac.instance.player.getPlayerClass(), selectedTile.getTilePosition(), new Vector2());
+//                        break;
+//                }
+//
+//                if (toBuild == null || Natac.instance.player.getGold() < cost) return;
+//
+//                buildSelector.remove();
+//                buildConfirmButton.remove();
+//                addActor(buildButton);
+//
+//                PacketUtil.buildStructure(toBuild, selectedTile.getTilePosition());
+//                PacketUtil.createStatChange(Natac.instance.player, 0, 0, 0, 0, -cost, 0);
+//            }
+//        };
+//    }
 
     private enum StructureSelection {
 
-        NONE("Select..."),
-        BARRACKS(Barracks.name + " ($" + Barracks.goldCost + ")");
+        BARRACKS("Barracks", 50, 0);
 
-        public final String label;
+        public final String name;
+        public final int resourceCost;
+        public final int goldCost;
 
-        public String toString() { return label; }
+        public String toString() {
+            String s = "";
+            s += name;
+            if (goldCost > 0) s += "($" + goldCost + ")";
+            if (resourceCost > 0) s += "(" + resourceCost + "R)";
+            return s;
+        }
 
-        StructureSelection(String label) {
-            this.label = label;
+        StructureSelection(String name, int goldCost, int resourceCost) {
+            this.name = name;
+            this.goldCost = goldCost;
+            this.resourceCost = resourceCost;
         }
     }
 }
