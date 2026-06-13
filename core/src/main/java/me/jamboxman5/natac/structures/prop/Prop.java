7461193f675f6fedb7b0d2ba789226b29a33b45e
Samuel Thompson <samthompson1818@gmail.com>
2026-06-13 13:36:49 -0400

    Added draw color placeholder for props without sprites

--- core/src/main/java/me/jamboxman5/natac/structures/prop/Prop.java
+++ core/src/main/java/me/jamboxman5/natac/structures/prop/Prop.java
@@ -1,11 +1,12 @@
 package me.jamboxman5.natac.structures.prop;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import me.jamboxman5.natac.structures.Structure;
 
 public class Prop extends Structure {
 
-    public Prop() {}
+    public Prop() { this.drawColor = Color.WHITE; }
 
     public Prop(Vector2 pos, Vector2 tilePos, String name) {
         super(0, 0, 0, tilePos, pos, name);
