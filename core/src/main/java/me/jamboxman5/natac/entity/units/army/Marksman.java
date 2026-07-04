06ed03b24c827e56b66a165094b6c12326ed4e94
DavidP0427 <dcordonejr@gmail.com>
2026-07-04 15:58:30 -0400

    Made the class files for the basic troops

---
+++ core/src/main/java/me/jamboxman5/natac/entity/units/army/Marksman.java
@@ -0,0 +1,39 @@
+package me.jamboxman5.natac.entity.units.army;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import me.jamboxman5.natac.entity.units.Unit;
+
+import java.util.UUID;
+
+public class Marksman extends Unit {
+
+    public static final int goldCost = 60;
+    public static final int resourceCost = 0;
+
+    public static final int attackCooldownMS = 1000;
+    public static final int baseDamage = 15;
+    public static final int attackForce = 50;
+    public static final int range = 30;
+
+    public static final float speed = 0.5f;
+    public static final int maxHealth = 50;
+
+    public Marksman() {
+        this.color = Color.GREEN;
+    }
+
+    public Marksman(Vector2 tilePos, Vector2 position, UUID owner) {
+        super(speed, range, maxHealth, baseDamage, attackCooldownMS, attackForce, tilePos, position, Color.CYAN, owner);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        if (sprite == null) {
+            initGraphics(new Texture(Gdx.files.internal("unit/placeholder_unit.png")), 0, 1);
+        }
+    }
+}
