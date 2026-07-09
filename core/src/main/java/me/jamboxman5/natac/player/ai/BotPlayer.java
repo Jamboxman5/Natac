a1a92d48b15e5b2308996971fecb69b0f331a4bf
Samuel Thompson <samthompson1818@gmail.com>
2026-07-09 15:41:08 -0400

    Added entity spawning for bot actions

--- core/src/main/java/me/jamboxman5/natac/player/ai/BotPlayer.java
+++ core/src/main/java/me/jamboxman5/natac/player/ai/BotPlayer.java
@@ -1,13 +1,16 @@
 package me.jamboxman5.natac.player.ai;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
 import me.jamboxman5.natac.Natac;
+import me.jamboxman5.natac.entity.structures.Structure;
+import me.jamboxman5.natac.entity.structures.constructed.Capital;
+import me.jamboxman5.natac.entity.units.Mob;
+import me.jamboxman5.natac.entity.units.army.Soldier;
 import me.jamboxman5.natac.map.Map;
 import me.jamboxman5.natac.map.tile.Tile;
 import me.jamboxman5.natac.net.DiscreteServer;
-import me.jamboxman5.natac.net.packet.Packet;
-import me.jamboxman5.natac.net.packet.PacketClaimTile;
-import me.jamboxman5.natac.net.packet.PacketEndTurn;
+import me.jamboxman5.natac.net.packet.*;
 import me.jamboxman5.natac.player.Player;
 import me.jamboxman5.natac.player.PlayerClass;
 
@@ -38,12 +41,15 @@ public class BotPlayer extends Player {
                 //BASE CASE
                 if (!m.ownsTiles(this)) {
                     Thread.sleep(getDelay() * 2);
-                    server.getServer().sendToAllTCP(claimBaseTile(m));
+                    claimBaseTile(server, m);
                     Thread.sleep(getDelay());
                     server.botEndTurn((PacketEndTurn) endTurn());
 
                 } else {
 
+                    //DO NOTHING
+                    Thread.sleep(getDelay() * 2);
+                    server.botEndTurn((PacketEndTurn) endTurn());
                 }
 
             } catch (InterruptedException e) {
@@ -73,20 +79,42 @@ public class BotPlayer extends Player {
         return classes[(int) (Math.random() * classes.length)];
     }
 
-    private Packet claimBaseTile(Map m) {
+    private void claimBaseTile(DiscreteServer server, Map m) {
         List<Tile> base = m.getBaseTiles();
         Tile claiming = base.get((int) (Math.random() * base.size()));
 
-        PacketClaimTile packet = new PacketClaimTile();
-        packet.claimingID = getID();
-        packet.tilePos = claiming.getTilePosition();
-        return packet;
-
+        server.getServer().sendToAllTCP(claimTile(claiming));
+        server.getServer().sendToAllTCP(buildStructure(claiming, new Capital(getPlayerClass(), claiming.getTilePosition()), true));
+        server.getServer().sendToAllTCP(spawnUnit(claiming, new Soldier(claiming.getTilePosition(), new Vector2(-20, -20), getID())));
+        server.getServer().sendToAllTCP(spawnUnit(claiming, new Soldier(claiming.getTilePosition(), new Vector2(20, -20), getID())));
     }
 
     private Packet endTurn() {
         PacketEndTurn packet = new PacketEndTurn();
         packet.turnPlayerID = getID();
+        return packet;
+    }
+
+    private Packet buildStructure(Tile tile, Structure building, boolean clearObstacles) {
+        PacketBuildStructure packet = new PacketBuildStructure();
+        packet.builderID = getID();
+        packet.tilePos = tile.getTilePosition();
+        packet.structure = building;
+        packet.clearObstacles = clearObstacles;
+        return packet;
+    }
+
+    private Packet spawnUnit(Tile tile, Mob spawning) {
+        PacketSpawnMob packet = new PacketSpawnMob();
+        packet.tilePos = tile.getTilePosition();
+        packet.mob = spawning;
+        return packet;
+    }
+
+    private Packet claimTile(Tile t) {
+        PacketClaimTile packet = new PacketClaimTile();
+        packet.claimingID = getID();
+        packet.tilePos = t.getTilePosition();
         return packet;
     }
 
